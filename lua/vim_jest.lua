89af885cca022bb00e53d18b2489f530a9894792
Matt Kubej <mkubej@gmail.com>
2020-09-27 21:07:23 -0400

    feat: ability to execute single file

--- lua/vim_jest.lua
+++ lua/vim_jest.lua
@@ -1,14 +1,26 @@
-local function jest()
+local function create_window()
   c_win = vim.api.nvim_get_current_win()
 
   vim.api.nvim_command('botright vnew')
   win = vim.api.nvim_get_current_win()
   buf = vim.api.nvim_get_current_buf()
+end
 
+local function test_project()
+  create_window()
   vim.api.nvim_command('terminal npx jest')
   vim.api.nvim_command('wincmd p')
 end
 
+local function test_current_file()
+  c_file = vim.api.nvim_eval('expand("%:p")')
+
+  create_window()
+  vim.api.nvim_command('terminal npx jest --runTestsByPath ' .. c_file .. ' --watch')
+  vim.api.nvim_command('wincmd p')
+end
+
 return {
-  jest = jest
+  testProject     = test_project,
+  testCurrentFile = test_current_file,
 }
--- plugin/vim_jest.vim
+++ plugin/vim_jest.vim
@@ -3,7 +3,8 @@ if exists('g:loaded_whid') | finish | endif
 let s:save_cpo = &cpo
 set cpo&vim
 
-command! Jest lua require'vim_jest'.jest()
+command! Jest lua require'vim_jest'.testProject()
+command! JestCurrent lua require'vim_jest'.testCurrentFile()
 
 let &cpo = s:save_cpo
 unlet s:save_cpo
