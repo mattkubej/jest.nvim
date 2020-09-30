6a4a7c466e9fabbd226f89ec639e14c53392b68b
Matt Kubej <mkubej@gmail.com>
2020-09-29 20:43:28 -0400

    refactor: utilize table concat for test_single_test

--- lua/vim_jest.lua
+++ lua/vim_jest.lua
@@ -28,7 +28,17 @@ local function test_single_test()
 
   if test_name ~= nil then
     create_window()
-    vim.api.nvim_command('terminal npx jest --runTestsByPath ' .. c_file .. " -t='" .. test_name .. "' --watch")
+
+    local t = {}
+    table.insert(t, 'terminal npx jest')
+    table.insert(t, ' --runTestsByPath ')
+    table.insert(t, c_file)
+    table.insert(t, " -t='")
+    table.insert(t, test_name)
+    table.insert(t, "' --watch")
+    jest_cmd = table.concat(t, '')
+
+    vim.api.nvim_command(jest_cmd)
     vim.api.nvim_command('wincmd p')
   end
 end
